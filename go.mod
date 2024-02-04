465eb45b1cfbf29b8435e5025e19df75e8ab74c3
Ara Israelyan <a.israelyan@gmail.com>
2024-02-04 15:15:16 +0200

    add MapScanner

--- go.mod
+++ go.mod
@@ -2,6 +2,15 @@ module github.com/juggle73/norm
 
 go 1.19
 
-require github.com/iancoleman/strcase v0.2.0
+require github.com/iancoleman/strcase v0.3.0
 
-require github.com/jackc/pgx/v5 v5.3.1
+require github.com/jackc/pgx/v5 v5.5.3
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ map_scanner.go
@@ -0,0 +1,66 @@
+package norm
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type MapScanner struct {
+	classes map[uint32]string
+}
+
+func NewMapScanner(conn *pgxpool.Pool) (*MapScanner, error) {
+
+	ms := &MapScanner{classes: make(map[uint32]string)}
+
+	rows, err := conn.Query(context.Background(),
+		"select oid, relname from pg_class")
+	if err != nil {
+		return nil, err
+	}
+
+	var (
+		oid  uint32
+		name string
+	)
+
+	for rows.Next() {
+		err = rows.Scan(&oid, &name)
+		if err != nil {
+			return nil, err
+		}
+		ms.classes[oid] = name
+	}
+
+	return ms, nil
+}
+
+func (ms *MapScanner) Scan(rows pgx.Rows) (map[string]any, error) {
+	descriptions := rows.FieldDescriptions()
+	res := make(map[string]any)
+
+	values, err := rows.Values()
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range descriptions {
+		tableName, ok := ms.classes[descriptions[i].TableOID]
+		if !ok {
+			return nil, errors.New(
+				fmt.Sprintf("table for oid %d not found", descriptions[i].TableOID))
+		}
+		m, ok := res[tableName]
+		if !ok {
+			m = make(map[string]any)
+			res[tableName] = m
+		}
+		mm := m.(map[string]any)
+		mm[descriptions[i].Name] = values[i]
+	}
+
+	return res, nil
+}
